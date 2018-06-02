1e80ebbc6611bbfa4f18d05acb0a6840fdf2bc5f
JDeLay-JR <jdelay.jr@gmail.com>
2018-06-01 22:50:06 -0400

    feat: added tinkle page and link

--- src/layouts/LandingPage/LandingPage.jsx
+++ src/layouts/LandingPage/LandingPage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import Link from 'gatsby-link'
 import tinkle from "../../../public/images/tinkle.png"
 import scrappr from "../../../public/images/scrappr.png"
-import me from "../../../public/images/about-me.jpg"
+import aboutMe from "../../../public/images/about-me.jpg"
 import blog from "../../../public/images/blog.png"
 import config from "../../../data/SiteConfig"
 import MainContent from "../../layouts/MainContent/MainContent"
@@ -17,10 +18,10 @@ class LandingPage extends Component {
             <img src={config.siteCover} className="img" alt="logo" />
           </div>
           <div className="landingPageComponent">
-            <img src={me} className="img" alt="me" />
+            <img src={aboutMe} className="img" alt="me" />
           </div>
           <div className="landingPageComponent">
-            <img src={tinkle} className="img" alt="tinkle" />
+            <Link to='/tinkle'><img src={tinkle} className="img" alt="tinkle" /></Link>
           </div>
           <div className="landingPageComponent">
             <p>Project: SBM</p>
---
+++ src/pages/tinkle.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import Link from "gatsby-link";
+
+class Tinkle extends Component {
+  render() {
+        return (
+          <div>
+            <p>Tinkle!</p>
+          </div>
+        )
+    }
+}
+
+export default Tinkle;
