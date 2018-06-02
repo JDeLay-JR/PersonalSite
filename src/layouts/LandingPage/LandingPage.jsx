aa41d66d571fee8724e33b8b8a2f2e04f4c3f5c3
JDeLay-JR <jdelay.jr@gmail.com>
2018-06-02 03:44:00 -0400

    feat: more layout stuff

--- src/layouts/LandingPage/LandingPage.jsx
+++ src/layouts/LandingPage/LandingPage.jsx
@@ -1,10 +1,4 @@
 import React, { Component } from "react";
-import Link from 'gatsby-link'
-import tinkle from "../../../public/images/tinkle.png"
-import scrappr from "../../../public/images/scrappr.png"
-import aboutMe from "../../../public/images/about-me.jpg"
-import blog from "../../../public/images/blog.png"
-import config from "../../../data/SiteConfig"
 import MainContent from "../../layouts/MainContent/MainContent"
 import "./LandingPage.css"
 
@@ -14,23 +8,25 @@ class LandingPage extends Component {
       
       <MainContent>
         <div className="landingPageContainer">
-          <div className="landingPageComponent">
-            <img src={config.siteCover} className="img" alt="logo" />
-          </div>
-          <div className="landingPageComponent">
-            <img src={aboutMe} className="img" alt="me" />
-          </div>
-          <div className="landingPageComponent">
-            <Link to='/tinkle' className="img"><img src={tinkle} className="img" alt="tinkle" /></Link>
-          </div>
-          <div className="landingPageComponent">
-            <p>Project: SBM</p>
-          </div>
-          <div className="landingPageComponent">
-            <img src={blog} className="img" alt="blog" />
-          </div>
-          <div className="landingPageComponent">
-            <img src={scrappr} className="img" alt="scrappr" />
+          <div className="landingPageSubContainer">
+            <div id="personalSite" className="landingPageComponent">
+              Personal Site
+            </div>
+            <div id="aboutMe" className="landingPageComponent">
+              About Me
+            </div>
+            <div id="tinkle" className="landingPageComponent">
+              Tinkle
+            </div>
+            <div id="sbm" className="landingPageComponent">
+              SBM
+            </div>
+            <div id="blog" className="landingPageComponent">
+              Blog
+            </div>
+            <div id="scrappr" className="landingPageComponent">
+              Scrappr
+            </div>
           </div>
         </div>
       </MainContent>
